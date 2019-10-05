a099e1c33dd26fde6609494e5769dfd1c2e16e0c
Junior <junior.jaava@gmail.com>
2019-10-05 16:57:08 +0200

    Login: - Send to server: user/password and getting a token from the server

--- instalura/src/componentes/Login.js
+++ instalura/src/componentes/Login.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
-    render(){ 
-            return (
-               <div className="login-box">
-               <h1 className="header-logo">Instalura</h1>
-               <form> 
-                <input type="text" />
-                <input type="password"/>
-                <input type="submit" value="login"/>
-            </form>
-        </div>
-     );
-  }   
+
+    constructor() {
+        super()
+        this.state = { msg: '' }
+    }
+
+    envia(event) {
+        event.preventDefault();
+        const resquetInfo = {
+            method: 'POST',
+            body: JSON.stringify({ login: this.login.value, senha: this.senha.value }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
+        fetch('https://instalura-api.herokuapp.com/api/public/login', resquetInfo)
+            .then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    this.setState({ msg: 'não foi possível fazer o login' })
+
+                }
+
+            })
+            .then(token => {
+                console.log(token);
+            })
+    }
+
+
+    render() {
+        return (
+            <div className="login-box">
+                <h1 className="header-logo">Instalura</h1>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.envia.bind(this)}>
+                    <input type="text" ref={(input) => this.login = input} />
+                    <input type="text" ref={(input) => this.senha = input} />
+                    <input type="submit" value="login" />
+                </form>
+            </div>
+        );
+    }
 }
