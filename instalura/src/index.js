63657ac5fedd7047a9a8bc7f80b550d2295d236e
Junior <junior.jaava@gmail.com>
2019-10-05 15:54:04 +0200

    Implementing login page. Features - BrowserRouter ( react-router-dom) -

--- instalura/src/index.js
+++ instalura/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/timeline.css';
 import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css';
 import App from './App';
+import Login from './componentes/Login';
+import {Route, BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <App/> , document.getElementById('root'));
+
+ReactDOM.render(
+    (
+        <BrowserRouter>
+            <Route path="/" component={Login}/>
+            <Route path="/timeline" component={App} />
+        </BrowserRouter>
+    ),
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
