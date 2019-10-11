1d3661946955326f4121774789187a47a80473f5
Junior <junior.jaava@gmail.com>
2019-10-11 14:40:34 +0200

    Make comments in posts. Using: - Pubsub.pusblish in FotoAtualizacao - Pubsub.subscribe in FotoInfo - Fetch post to send a new comments

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -23,12 +23,11 @@ class FotoHeader extends Component {
 
 class FotoInfo extends Component {
 
-
     constructor(props) {
         super(props);
-        this.state = { likers: this.props.foto.likers };
-
+        this.state = { likers: this.props.foto.likers, comentarios: this.props.foto.comentarios };
     }
+
     componentWillMount() {
         Pubsub.subscribe('atualiza-liker', (topico, infoLiker) => {
             if (this.props.foto.id === infoLiker.fotoId) { //all the subscribers receive the message. So we need to identifier the right one
@@ -41,7 +40,14 @@ class FotoInfo extends Component {
                     this.setState({ likers: novosLikers });
                 }
             }
-        })
+        });
+        Pubsub.subscribe('novos-comentarios', (topico, infoComentario) => {
+            if (this.props.foto.id === infoComentario.fotoId) {
+                const novosComentarios = this.state.comentarios.concat(infoComentario.novoComentario);
+                this.setState({ comentarios: novosComentarios });
+            }
+            console.log(infoComentario);
+        });
     }
 
     render() {
@@ -63,7 +69,7 @@ class FotoInfo extends Component {
 
                 <ul className="foto-info-comentarios">
                     {
-                        this.props.foto.comentarios.map(comentario => {
+                        this.state.comentarios.map(comentario => {
                             return (
                                 <li className="comentario" key={comentario.id}>
                                     <Link to={`/timeline/${comentario.login} `} className="foto-info-autor">{comentario.login} </Link>
@@ -104,13 +110,38 @@ class FotoAtualizacoes extends Component {
             });
     }
 
+
+
+    comenta(event) {
+        event.preventDefault();
+
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({ texto: this.comentario.value }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
+        fetch(`https://instalura-api.herokuapp.com/api/fotos/${this.props.foto.id}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`, requestInfo)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error("não foi possível comentar");
+                }
+            }).then(novoComentario => {
+                Pubsub.publish('novos-comentarios', { fotoId: this.props.foto.id, novoComentario });
+            })
+    }
+
     render() {
         return (
             <section className="fotoAtualizacoes">
                 <a onClick={this.like.bind(this)} className={this.state.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Linkar</a>
                 <form className="fotoAtualizacoes-form"></form>
-                <form className="fotoAtualizacoes-form">
-                    <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" />
+                <form className="fotoAtualizacoes-form" onSubmit={this.comenta.bind(this)}>
+                    <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comentario = input} />
                     <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit" />
                 </form>
             </section>
